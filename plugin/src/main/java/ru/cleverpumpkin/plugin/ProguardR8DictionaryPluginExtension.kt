9a8a571ad2408e8bd96e3596ffc6fcac87ce25eb
Ruslan Arslanov <arslanov.r.f@gmail.com>
2019-07-18 13:06:14 +0300

    Increase the number of generated lines to 30000; min line length – to 10

--- plugin/src/main/java/ru/cleverpumpkin/plugin/ProguardR8DictionaryPluginExtension.kt
+++ plugin/src/main/java/ru/cleverpumpkin/plugin/ProguardR8DictionaryPluginExtension.kt
@@ -8,9 +8,9 @@ open class ProguardR8DictionaryPluginExtension {
     companion object {
         internal const val NAME = "proguardDictionaries"
 
-        const val DEFAULT_LINES_COUNT = 1000
-        const val DEFAULT_MIN_LINE_LENGTH = 5
-        const val DEFAULT_MAX_LINE_LENGTH = 20
+        const val DEFAULT_LINES_COUNT = 30000
+        const val DEFAULT_MIN_LINE_LENGTH = 10
+        const val DEFAULT_MAX_LINE_LENGTH = 30
     }
 
     var dictionaryNames = listOf<String>()
